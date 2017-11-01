f8507f6cc0e3ddbc2a518c89796d5c3fba2d3439
Aaron Levine <ald.levine@gmail.com>
2017-10-31 23:44:03 -0400

    Built transcoder and tests

--- packages/transcoder/transcoder.js
+++ packages/transcoder/transcoder.js
@@ -1,7 +1,8 @@
 const {Duplex, PassThrough} = require('stream');
 
 /**
- * A class representing the base transcoder mechanism
+ * A class representing the base transcoder mechanism. It wraps another duplex
+ * stream and encodes data when written and decodes data when read.
  * @extends {stream~Duplex}
  */
 module.exports = class Transcoder extends Duplex
@@ -9,16 +10,43 @@ module.exports = class Transcoder extends Duplex
   /**
    * Creates a transcoder.
    * @param {object} [opts={}] - The configuration options.
-   * @param {stream~Transform} [opts.encoder=new stream.PassThrough] - The encoding transform
-   * @param {stream~Transform} [opts.decoder=new stream.PassThrough] - The decoding transform
+   * @param {stream~Duplex} - The connected stream.
+   * @param {stream~Transform} [opts.encoder=new stream.PassThrough] - The encoding transform.
+   * @param {stream~Transform} [opts.decoder=new stream.PassThrough] - The decoding transform.
    */
   constructor ({
+    stream,
     encoder = new PassThrough(),
     decoder = new PassThrough(),
-  }={})
+  }={}, opts = {
+    writableObjectMode: true,
+    readableObjectMode: true,
+  })
   {
-    super();
+    super(opts);
+
+    if (!stream) throw TypeError('Expected stream option');
+
+    /** The connectied stream */
+    this._stream = stream;
+
+    /** The encoding transform */
     this._encoder = encoder;
+
+    /** The decoding transform */
     this._decoder = decoder;
+
+    encoder.pipe(stream);
+    stream.pipe(decoder);
+  }
+
+  _read (size)
+  {
+    this.push(this._decoder.read(size));
+  }
+
+  _write (chunk, enc, cb)
+  {
+    this._encoder.write(chunk, enc, cb);
   }
 }
--- test/transcoder/transcoder.js
+++ test/transcoder/transcoder.js
@@ -1,7 +1,122 @@
 const test = require('ava');
+const {Transform, PassThrough} = require('stream');
+const net = require('net');
+const Mitm = require('mitm');
 const Transcoder = require('../../packages/transcoder');
 
-test('noop', (t) => {
-  t.pass();
+const decoded = {foo: 'bar', baz: 'bat'};
+const encoded = '{"foo":"bar","baz":"bat"}';
+
+test.beforeEach((t) => {
+
+  t.context.encoder = new Transform({
+    writableObjectMode: true,
+    transform (chunk, enc, cb) {
+      this.push(JSON.stringify(chunk));
+      cb();
+    }
+  });
+
+  t.context.decoder = new Transform({
+    readableObjectMode: true,
+    transform (chunk, enc, cb) {
+      this.push(JSON.parse(chunk));
+      cb();
+    }
+  });
+
+  t.context.mitm = Mitm();
 });
 
+test.afterEach((t) => {
+  t.context.mitm.disable();
+});
+
+test('Requires stream option', (t) => {
+  t.throws(() => {
+    new Transcoder();
+  }, TypeError);
+});
+
+test('Allows defaults transforms', (t) => {
+  const socket = net.connect();
+  const transcoder = new Transcoder({stream: socket});
+
+  t.truthy(transcoder instanceof Transcoder);
+});
+
+test('Encodes correctly', (t) => {
+  return new Promise((res, rej) => {
+    const {mitm, encoder, decoder} = t.context;
+
+    mitm.on('connection', (socket) => {
+      socket.on('data', (data) => {
+        t.is(data.toString(), encoded);
+        res();
+      })
+    });
+
+    const socket = net.connect();
+
+    const transcoder = new Transcoder({
+      stream: socket,
+      encoder: encoder,
+      decoder: decoder
+    });
+
+    transcoder.write(decoded);
+  });
+});
+
+test('Decodes correctly', (t) => {
+  return new Promise((res, rej) => {
+    const {mitm, encoder, decoder} = t.context;
+
+    mitm.on('connection', (socket) => {
+      socket.write(encoded);
+    });
+
+    const socket = net.connect();
+
+    const transcoder = new Transcoder({
+      stream: socket,
+      encoder: encoder,
+      decoder: decoder
+    });
+
+    transcoder.on('data', (data) => {
+      t.deepEqual(data, decoded);
+      res();
+    });
+  });
+});
+
+test('Transcodes correctly', (t) => {
+  return new Promise((res, rej) => {
+    const {mitm, encoder, decoder} = t.context;
+
+    mitm.on('connection', (socket) => {
+      const transcoder = new Transcoder({
+        stream: socket,
+        encoder: encoder,
+        decoder: decoder
+      });
+      transcoder.write(decoded);
+    });
+
+    const socket = net.connect();
+
+    const transcoder = new Transcoder({
+      stream: socket,
+      encoder: encoder,
+      decoder: decoder
+    });
+
+    transcoder.on('data', (data) => {
+      t.deepEqual(data, decoded);
+      res();
+    });
+
+    transcoder.write(decoded);
+  });
+});
